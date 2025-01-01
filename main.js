273811c86c70688f58fccb5a40cb3a0d8dca353c
donjd <99505859+donjd@users.noreply.github.com>
2025-01-01 16:22:09 -0700

    Link all HTML elements to JS

---
+++ main.js
@@ -0,0 +1,35 @@
+//display
+const history = document.querySelector("#input-history");
+const display = document.querySelector("#display");
+
+//row one buttons
+const leftParenthesis = document.querySelector("#left-parenthesis-btn");
+const rigthParenthesis = document.querySelector("#right-parenthesis-btn");
+const percent = document.querySelector("#percent-btn");
+const clearBtn = document.querySelector("#clear-btn");
+
+//row two buttons
+const seven = document.querySelector("#seven-btn");
+const eight = document.querySelector("#eight-btn");
+const nine = document.querySelector("#nine-btn");
+const divideBtn = document.querySelector("#divide-btn");
+
+//row three buttons
+const four = document.querySelector("#four-btn");
+const five = document.querySelector("#five-btn");
+const six = document.querySelector("#six-btn");
+const multiplyBtn = document.querySelector("#multiply-btn");
+
+//row four buttons
+const one = document.querySelector("#one-btn");
+const two = document.querySelector("#two-btn");
+const three = document.querySelector("#three-btn");
+const minusBtn = document.querySelector("#minus-btn");
+
+//row five buttons
+const decimal = document.querySelector("#decimal-btn");
+const zero = document.querySelector("#zero-btn");
+const equal = document.querySelector("#equal-btn");
+const plusBtn = document.querySelector("#plus-btn");
+
+//math functions
